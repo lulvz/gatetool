dd86efaf061c95cfc81e8365ce46c69ef2ed4cdc
lulascoca <lulascocainfinity@gmail.com>
2020-01-27 21:18:22 +0000

    a

--- scripts_waves/honeoutsidedoor.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/sh
-
-sudo rpitx -f 433924 -i waves/48testoutsidehouse.wav -m IQ
-
-#replace this file with the rpitx wavefiles or scripts that use the
--- scripts_waves/inside_door_hone.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/sh
-
-sudo rpitx -f 433917 -i waves/48inside_door_hone.wav -m IQ
-
-#replace this file with the rpitx wavefiles or scripts that use the
